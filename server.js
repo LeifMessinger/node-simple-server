a23bf3b6fab2d0a30dc9e6ffba4d71d82e890726
Leif Messinger <leifmessinger@gmail.com>
2020-05-15 22:25:29 -0500

    Made https-noframe
    Template can be found at noframe-template

--- server.js
+++
@@ -1,7 +0,0 @@
-console.log('Starting...');
-const http = require('http');
-const port = process.env.PORT || process.argv[2] || 8080;
-const simple = require(__dirname + '/simple-server');
-const server = http.createServer(simple.sauce);
-server.listen(port);
-console.log('Started at ' + port);
--- subfolder/script.js
+++
@@ -1,1 +0,0 @@
-alert("Subfolder js works!")
