c4475cea1c1244370bc4930a7433871a4a4c4a24
Leif Messinger <59458630+LeifMessinger@users.noreply.github.com>
2020-05-22 16:59:01 -0500

    Server Side Stuff

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ const simple = require('noframe-http');
 const server = http.createServer(simple.sauce);
 //Attach Socket.io
 const io = require('socket.io')(server);
+var connections = [];
 io.on('connection', function(socket){
+	connections.push(socket);
+	socket.on('disconnect', function(reason){
+		connections.splice(connections.indexOf(socket),1);
+	});
 	socket.authCode = Math.floor(Math.random()*9999);
 	socket.emit('connected',socket.authCode);
 	let count = 0;
@@ -16,9 +21,9 @@ io.on('connection', function(socket){
 	});
 	console.log("Connected: " + count + " clients.")
 });
-/*io.on('disconnect', function(){
-	
-});*/
+io.on('authenticate', function(code){
+	for(let c in connections) if(connections[c].authCode == code) connections[c].emit('authenticated');
+});
 //Start Server
 server.listen(port);
 console.log('Started at ' + port);
